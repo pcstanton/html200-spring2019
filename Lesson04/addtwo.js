02a9124ee01f6cfc5f263d18d7b6e463f5278938
Patrick Stanton <atrista@microsoft.com>
2019-04-26 13:35:08 -0700

    lesson 4, challenge 1 + bonus

---
+++ Lesson04/addtwo.js
@@ -0,0 +1,10 @@
+function numberAddtwo() {
+  let input = prompt('Enter a Number');
+  let input2 = prompt('Enter a Second Number');
+  let result = 0;
+
+  result= Number(input) + Number(input2);
+
+  alert(result);
+
+}
---
+++ Lesson04/square.js
@@ -0,0 +1,8 @@
+function numberSquare() {
+  let input = prompt('Enter a Number');
+  let result = 0;
+
+  result = Number(input*input);
+
+  alert(result);
+}
